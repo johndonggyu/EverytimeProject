13c7ee1c6d48b1a891109f89373096a700b5a5ca
elenajso <elenajso.810@gmail.com>
2019-08-17 16:52:45 +0900

    비밀번호 변경 완성 영어 글씨체 변경 회원가입 에러 떠서 views.py 수정함

--- Web/views.py
+++ Web/views.py
@@ -108,7 +108,7 @@ class join(View):
 			user.save()
 			current_site = get_current_site(request)
 			#localhost:8000
-			uid = urlsafe_base64_encode(force_bytes(user.pk)).encode().decode()
+			uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
 			token = account_activation_token.make_token(user)
 			message = render_to_string('user_activate_email.html', {
 				'user':user,
--- home/settings.py
+++ home/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ko'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
